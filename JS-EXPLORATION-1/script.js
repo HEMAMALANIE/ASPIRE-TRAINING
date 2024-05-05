f30a1fc70aa2536dd284ae0aa7bb6cdecb05ef40
HEMAMALANIE <140788869+HEMAMALANIE@users.noreply.github.com>
2024-05-05 09:08:59 +0530

    Delete JS-EXPLORATION-1 directory

--- JS-EXPLORATION-1/script.js
+++
@@ -1,9 +0,0 @@
-// Get the button and paragraph elements
-const button = document.getElementById("myButton");
-const paragraph = document.getElementById("myParagraph");
-
-// Add an event listener to the button
-button.addEventListener("click", function() {
-    // Change the text of the paragraph when the button is clicked
-    paragraph.textContent = "Button clicked! Text changed.";
-});
