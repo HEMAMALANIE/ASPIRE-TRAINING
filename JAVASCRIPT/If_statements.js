c1b461eacf4f9a042eea1a68b014a0d809230819
HEMAMALANIE <140788869+HEMAMALANIE@users.noreply.github.com>
2024-05-05 09:34:38 +0530

    Add files via upload

---
+++ JAVASCRIPT/If_statements.js
@@ -0,0 +1,11 @@
+let viewer="Have I got placed";
+let reply;
+if(viewer)
+    {
+        reply=`Enjoy You ${viewer}`;
+    }
+    else
+    {
+        reply=`Sorry Better opportunity nexr time`;
+    }
+console.log(reply);
